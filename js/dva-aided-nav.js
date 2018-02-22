226d863b3a76410611507aced82c700c7d56de12
DVASTAFF\ccampb <bennyc2444@hotmail.com>
2018-02-23 08:30:05 +1100

    implemented aided nav js and updated menu.css

--- js/dva-aided-nav.js
+++ js/dva-aided-nav.js
@@ -1,12 +1,11 @@
-(function ($) {
-    Drupal.behaviors.exampleModule = {
-      attach: function (context, settings) {
-         $('.sf-menu-aided-navigation .menuparent').unbind('mouseover');
-         $('.sf-menu-aided-navigation .menuparent').click(function(e) {
-            e.preventDefault();
-            event.stopPropagation();
-      })
-    }
-}
-    
-  }(jQuery));
+(function($) {
+  $(window).bind('load', function(){
+  $('.sf-menu-aided-navigation .menuparent').unbind('mouseover');
+  $('.sf-menu-aided-navigation .menuparent').unbind('mouseleave');
+  $('.sf-menu-aided-navigation .menuparent').on('click', function(event) {
+  $('ul').removeClass('sf-hidden').addClass('sf-menu-aided-navigation-new');
+     event.preventDefault();
+})
+    });
+  
+}(jQuery));
